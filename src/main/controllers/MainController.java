7586f74444628c300a0b7991e0b5bea4015ef52f
widuch.karol <karol_widuch@interia.pl>
2020-05-20 16:34:02 +0200

    Basic main and start screen

--- src/main/controllers/MainController.java
+++ src/main/controllers/MainController.java
@@ -1,6 +1,26 @@
 package main.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
 //Controller for main window
 public class MainController {
+    @FXML
+    private StackPane mainStackPane;
 
+    @FXML
+    public void initialize(){
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/main/resources/layouts/StartScreen.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainStackPane.getChildren().add(pane);
+    }
 }
---
+++ src/main/controllers/StartScreenController.java
@@ -0,0 +1,12 @@
+package main.controllers;
+
+import javafx.fxml.FXML;
+
+public class StartScreenController {
+
+    @FXML
+    public void openSettings(){
+//        System.out.println("witam");
+    }
+
+}
